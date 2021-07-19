272edd91f01042c166762ba4084a78a500b1e3f4
miuser <miuser@email.com>
2021-07-19 16:15:11 +0800

    T704

---
+++ src/main/java/com/github/makewheels/algorithm/leetcode/T374.java
@@ -0,0 +1,11 @@
+package com.github.makewheels.algorithm.leetcode;
+
+public class T374 {
+    public int guess(int num) {
+        return 0;
+    }
+
+    public int guessNumber(int n) {
+        return 0;
+    }
+}
