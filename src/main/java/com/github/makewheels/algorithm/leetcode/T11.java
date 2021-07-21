2de398561f59e50b0077a2ddd580c63ab0355bd3
miuser <miuser@email.com>
2021-07-21 16:26:58 +0800

    T11盛满水

---
+++ src/main/java/com/github/makewheels/algorithm/leetcode/T11.java
@@ -0,0 +1,33 @@
+package com.github.makewheels.algorithm.leetcode;
+
+public class T11 {
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+        int maxLeft = left;
+        int maxRight = right;
+        while (left < right) {
+            int currentHeight = Math.min(height[left], height[right]);
+            int distance = right - left;
+            int area = currentHeight * distance;
+            if (area > maxArea) {
+                maxArea = area;
+                maxLeft = left;
+                maxRight = right;
+            }
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        int currentHeight = Math.min(height[maxLeft], height[maxRight]);
+        int distance = maxRight - maxLeft;
+        return currentHeight * distance;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7};
+        System.out.println(new T11().maxArea(arr));
+    }
+}
