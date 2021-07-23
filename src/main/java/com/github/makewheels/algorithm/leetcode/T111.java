280c2c7ef7bc91ad9bd9147941bdffc4cb85a2d2
miuser <miuser@email.com>
2021-07-23 16:03:59 +0800

    T111求二叉树高度

--- src/main/java/com/github/makewheels/algorithm/leetcode/T111.java
+++ src/main/java/com/github/makewheels/algorithm/leetcode/T111.java
@@ -2,10 +2,17 @@ package com.github.makewheels.algorithm.leetcode;
 
 public class T111 {
     public int minDepth(TreeNode root) {
-        return 0;
+
+        if (root == null)
+            return 0;
+        if (root.left == null && root.right == null)
+            return 1;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        if (root.left == null || root.right == null)
+            return left + right + 1;
+        return Math.min(left, right) + 1;
+
     }
 
-    public static void main(String[] args) {
-
-    }
 }
