55bffbfa3fa50bff621d24d2f5148c4aca3c0719
miuser <miuser@email.com>
2021-08-03 20:30:50 +0800

    T189旋转数组

---
+++ src/main/java/com/github/makewheels/algorithm/leetcode/T189.java
@@ -0,0 +1,14 @@
+package com.github.makewheels.algorithm.leetcode;
+
+public class T189 {
+    public void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        if (k == 0 || k == nums.length)
+            return;
+        int[] arr = new int[nums.length];
+        System.arraycopy(nums, 0, arr, k, nums.length - k);
+        System.arraycopy(nums, nums.length - k, arr, 0, k);
+        System.arraycopy(arr, 0, nums, 0, nums.length);
+
+    }
+}
