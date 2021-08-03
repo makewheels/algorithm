35957ef0885fd2896cd67e19b2b916092c5afeff
makewheels <makewheels>
2021-08-03 10:11:28 +0800

    T203 删除链表节点

--- src/main/java/com/github/makewheels/algorithm/leetcode/T203.java
+++ src/main/java/com/github/makewheels/algorithm/leetcode/T203.java
@@ -4,17 +4,36 @@ import com.github.makewheels.algorithm.bean.ListNode;
 
 public class T203 {
     public ListNode removeElements(ListNode head, int val) {
-        ListNode each = head;
+        //没有节点
+        if (head == null)
+            return null;
+        //只有一个节点
+        if (head.next == null) {
+            if (head.val == val) {
+                head = null;
+            }
+            return head;
+        }
+
+        //有两个节点
+        //解决头结点
+        while (head != null && head.val == val) {
+            head = head.next;
+        }
+        if (head == null)
+            return null;
+        //头结点不是要删的了
+        ListNode pre = head;
+        ListNode each = head.next;
         while (each != null) {
+            boolean isDelete = false;
             if (each.val == val) {
-                if (each.next == null) {
-                    each = null;
-                } else {
-                    each.next = each.next.next;
-                    continue;
-                }
+                pre.next = each.next;
+                isDelete = true;
             }
             each = each.next;
+            if (!isDelete)
+                pre = pre.next;
         }
         return head;
     }
