64f64fffe411b17adb8ff6b68b8a6fd97a1d31bd
makewheels <makewheels>
2021-08-03 23:54:27 +0800

    J46 解析字母，动态规划

--- src/main/java/com/github/makewheels/algorithm/leetcode/J46.java
+++ src/main/java/com/github/makewheels/algorithm/leetcode/J46.java
@@ -3,7 +3,17 @@ package com.github.makewheels.algorithm.leetcode;
 public class J46 {
     public int translateNum(int num) {
         char[] chars = (num + "").toCharArray();
-
-        return 0;
+        int[] dp = new int[chars.length + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for (int i = 1; i < chars.length; i++) {
+            dp[i + 1] = dp[i];
+            int currentNumber = Integer.parseInt(chars[i - 1] + String.valueOf(chars[i]));
+            System.out.println(currentNumber);
+            if (currentNumber >= 10 && currentNumber <= 25) {
+                dp[i + 1] = dp[i] + dp[i - 1];
+            }
+        }
+        return dp[chars.length];
     }
 }
