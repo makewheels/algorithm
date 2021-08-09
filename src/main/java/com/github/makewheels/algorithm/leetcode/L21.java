c2884491b38245b1a4c2fe4ad857f3baece13d5e
miuser <miuser@email.com>
2021-08-09 17:25:36 +0800

    合并两个有序链表

---
+++ src/main/java/com/github/makewheels/algorithm/leetcode/L21.java
@@ -0,0 +1,39 @@
+package com.github.makewheels.algorithm.leetcode;
+
+import com.github.makewheels.algorithm.bean.ListNode;
+
+public class L21 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode head = null;
+        ListNode each = null;
+        while (l1 != null || l2 != null) {
+            int value;
+            if (l1 == null) {
+                value = l2.val;
+                l2 = l2.next;
+            } else if (l2 == null) {
+                value = l1.val;
+                l1 = l1.next;
+            } else {
+                if (l1.val < l2.val) {
+                    value = l1.val;
+                    l1 = l1.next;
+                } else {
+                    value = l2.val;
+                    l2 = l2.next;
+                }
+            }
+
+            if (head == null) {
+                head = new ListNode(value);
+                each = head;
+            } else {
+                each.next = new ListNode(value);
+                each = each.next;
+            }
+
+        }
+        return head;
+    }
+
+}
