1859fcd3cfb37339f3af78f5376add02ae4271b8
makewheels <makewheels>
2021-08-09 11:37:12 +0800

    两数相加

--- src/main/java/com/github/makewheels/algorithm/leetcode/L2.java
+++ src/main/java/com/github/makewheels/algorithm/leetcode/L2.java
@@ -6,7 +6,9 @@ public class L2 {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
         ListNode head = null;
-        ListNode tail = null;
+        ListNode each = null;
+        boolean carry = false;
+
         while (l1 != null || l2 != null) {
             int value1 = 0;
             if (l1 != null)
@@ -14,16 +16,28 @@ public class L2 {
             int value2 = 0;
             if (l2 != null)
                 value2 = l2.val;
+
             int sum = value1 + value2;
             if (head == null) {
                 head = new ListNode(sum % 10);
+                each = head;
             } else {
-                head.next = new ListNode(sum % 10);
+                if (carry)
+                    sum++;
+
+                each.next = new ListNode(sum % 10);
+                each = each.next;
             }
+
+            carry = sum >= 10;
+
             if (l1 != null)
                 l1 = l1.next;
             if (l2 != null)
                 l2 = l2.next;
+        }
+        if (carry) {
+            each.next = new ListNode(1);
         }
         return head;
 
