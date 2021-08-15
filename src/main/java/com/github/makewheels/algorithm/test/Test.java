dcfd90190dc40018755e59f00e784b0c85d671f4
makewheels <makewheels>
2021-08-15 15:28:15 +0800

    getClassLoader

---
+++ src/main/java/com/github/makewheels/algorithm/test/Test.java
@@ -0,0 +1,8 @@
+package com.github.makewheels.algorithm.test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(Class.class.getClassLoader());
+        System.out.println(Object.class.getClassLoader());
+    }
+}
