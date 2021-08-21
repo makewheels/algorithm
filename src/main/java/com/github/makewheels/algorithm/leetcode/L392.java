fa697d9df2599c9d4d5183676b0565da9936fa47
miuser <miuser@email.com>
2021-08-21 17:54:23 +0800

    T392字符串子序列

---
+++ src/main/java/com/github/makewheels/algorithm/leetcode/L392.java
@@ -0,0 +1,15 @@
+package com.github.makewheels.algorithm.leetcode;
+
+public class L392 {
+    public boolean isSubsequence(String s, String t) {
+        int p1 = 0;
+        int p2 = 0;
+        while (p1 < s.length() && p2 < t.length()) {
+            if (s.charAt(p1) == t.charAt(p2)) {
+                p1++;
+            }
+            p2++;
+        }
+        return p1 == s.length();
+    }
+}
