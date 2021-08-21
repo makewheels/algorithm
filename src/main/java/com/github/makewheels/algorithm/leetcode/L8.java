768d64aa09e85d2d00d62892f72c4005c3a85c9f
makewheels <makewheels>
2021-08-22 00:04:21 +0800

    getClassLoader

---
+++ src/main/java/com/github/makewheels/algorithm/leetcode/L8.java
@@ -0,0 +1,10 @@
+package com.github.makewheels.algorithm.leetcode;
+
+public class L8 {
+    public int myAtoi(String s) {
+        while (s.startsWith(" ")) {
+            s = s.substring(1);
+        }
+
+    }
+}
