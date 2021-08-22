36f18d5398b535c5daba6db712652020804569fa
miuser <miuser@email.com>
2021-08-22 17:37:34 +0800

    L415字符串数字相加

--- src/main/java/com/github/makewheels/algorithm/leetcode/L415.java
+++ src/main/java/com/github/makewheels/algorithm/leetcode/L415.java
@@ -2,45 +2,19 @@ package com.github.makewheels.algorithm.leetcode;
 
 public class L415 {
     public String addStrings(String num1, String num2) {
-        int p1 = num1.length() - 1;
-        int p2 = num2.length() - 1;
-        StringBuilder result = new StringBuilder(
-                Math.max(num1.length(), num2.length()) + 1);
-
-        boolean carry = false;
-        while (p1 >= 0 || p2 >= 0) {
-            if (p1 < 0) {
-                if (carry) {
-                    result.append(num2.charAt(p2) + 1);
-                    carry = (num2.charAt(p2) + 1) >= 10;
-                } else {
-                    result.append(num2.charAt(p2));
-                }
-            } else if (p2 < 0) {
-                if (carry) {
-                    result.append(num1.charAt(p1) + 1);
-                    carry = (num2.charAt(p1) + 1) >= 10;
-                } else {
-                    result.append(num1.charAt(p1));
-                }
-            } else {
-                int value = Integer.parseInt(num1.charAt(p1) + "") +
-                        Integer.parseInt(num2.charAt(p2) + "");
-                if (carry) {
-                    value++;
-                    carry = false;
-                }
-                result.append(value % 10);
-                if (value >= 10)
-                    carry = true;
-            }
-            p1--;
-            p2--;
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int add = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+        while (i >= 0 || j >= 0 || add != 0) {
+            int x = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int y = j >= 0 ? num2.charAt(j) - '0' : 0;
+            int temp = x + y + add;
+            stringBuilder.append(temp % 10);
+            add = temp / 10;
+            i--;
+            j--;
         }
-        if (carry) {
-            result.append("1");
-        }
-
-        return result.reverse().toString();
+        return stringBuilder.reverse().toString();
     }
 }
