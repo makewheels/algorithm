3ff5fee7ba472acd5c419e24ad5461d664df589f
miuser <miuser@email.com>
2021-08-22 17:58:14 +0800

    删除有序数组重复元素

---
+++ src/main/java/com/github/makewheels/algorithm/leetcode/L26.java
@@ -0,0 +1,16 @@
+package com.github.makewheels.algorithm.leetcode;
+
+public class L26 {
+    public int removeDuplicates(int[] nums) {
+        int p1 = 0;
+        int p2 = 1;
+        while (p2 < nums.length) {
+            if (nums[p1] != nums[p2]) {
+                p1++;
+                nums[p1] = nums[p2];
+            }
+            p2++;
+        }
+        return p1 + 1;
+    }
+}
