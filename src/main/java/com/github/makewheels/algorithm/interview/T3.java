b5b5c86542011da6479ca757ae2270b822a23c03
makewheels <makewheels>
2021-08-28 10:35:26 +0800

    aiebe

---
+++ src/main/java/com/github/makewheels/algorithm/interview/T3.java
@@ -0,0 +1,29 @@
+package com.github.makewheels.algorithm.interview;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class T3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line1 = scanner.nextLine();
+        String line2 = scanner.nextLine();
+        String[] s1 = line1.split(";");
+        String[] s2 = line2.split(";");
+        Map<String, String> map1 = new LinkedHashMap<>();
+        for (String s : s1) {
+            String[] split = s.split(",");
+            map1.put(split[0].trim(), split[1]);
+        }
+        Map<String, String> map2 = new HashMap<>();
+        for (String s : s2) {
+            String[] split = s.split(",");
+            map2.put(split[0].trim(), split[1]);
+        }
+        for (String str : map1.keySet()) {
+            System.out.println(str + ",['" + map2.get(map1.get(str)) + "']");
+        }
+    }
+}
