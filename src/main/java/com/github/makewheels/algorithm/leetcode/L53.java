be5c17c46f15a042eb9b15874afb5401870face1
miuser <miuser@email.com>
2021-08-28 11:04:58 +0800

    LC

---
+++ src/main/java/com/github/makewheels/algorithm/leetcode/L53.java
@@ -0,0 +1,7 @@
+package com.github.makewheels.algorithm.leetcode;
+
+public class L53 {
+    public int maxSubArray(int[] nums) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/github/makewheels/algorithm/test/Test.java
@@ -0,0 +1,10 @@
+package com.github.makewheels.algorithm.test;
+
+public class Test {
+    public static void main(String[] args) {
+        Integer integer1 = new Integer(1);
+        Integer integer2 = new Integer(1);
+        System.out.println(integer1 .equals(integer2));
+
+    }
+}
