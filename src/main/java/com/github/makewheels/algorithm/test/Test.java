0a9fa65e896933c98812dedd4c9ba373d5ac3c49
miuser <miuser@email.com>
2021-08-28 12:20:25 +0800

    Test

--- src/main/java/com/github/makewheels/algorithm/test/Test.java
+++ src/main/java/com/github/makewheels/algorithm/test/Test.java
@@ -1,10 +1,17 @@
 package com.github.makewheels.algorithm.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
-        Integer integer1 = new Integer(1);
-        Integer integer2 = new Integer(1);
-        System.out.println(integer1 .equals(integer2));
-
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        System.out.println(list);
+        list.remove(new Integer(1));
+        System.out.println(list);
     }
 }
