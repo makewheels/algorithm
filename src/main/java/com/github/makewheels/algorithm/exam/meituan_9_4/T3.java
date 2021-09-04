c745e498e467d349ce9392c7581ecd8f5e5fb084
miuser <miuser@email.com>
2021-09-04 17:55:43 +0800

    美团笔试

---
+++ src/main/java/com/github/makewheels/algorithm/exam/meituan_9_4/T3.java
@@ -0,0 +1,7 @@
+package com.github.makewheels.algorithm.exam.meituan_9_4;
+
+public class T3 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/github/makewheels/algorithm/exam/meituan_9_4/T5.java
@@ -0,0 +1,17 @@
+package com.github.makewheels.algorithm.exam.meituan_9_4;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class T5 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(1);
+        list.add(8);
+        int index = random.nextInt(list.size());
+        System.out.println(list.get(index));
+    }
+}
