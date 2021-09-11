82a522a0047107bdd6598b866eaa08ad6328ba55
makewheels <makewheels>
2021-09-11 14:56:48 +0800

    天翼云笔试

---
+++ src/main/java/com/github/makewheels/algorithm/exam/tianyiyun_9_11/T2.java
@@ -0,0 +1,48 @@
+package com.github.makewheels.algorithm.exam.tianyiyun_9_11;
+
+import com.sun.media.sound.SoftTuning;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class T2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        } else if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+        int[] arr = {1, 5, 10, 20, 50, 100};
+        List<List<Integer>> list = combinationSum(arr, n);
+        System.out.println(list.size());
+    }
+
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> combine = new ArrayList<Integer>();
+        dfs(candidates, target, ans, combine, 0);
+        return ans;
+    }
+
+    public static void dfs(int[] candidates, int target, List<List<Integer>> ans, List<Integer> combine, int idx) {
+        if (idx == candidates.length) {
+            return;
+        }
+        if (target == 0) {
+            ans.add(new ArrayList<Integer>(combine));
+            return;
+        }
+        dfs(candidates, target, ans, combine, idx + 1);
+        if (target - candidates[idx] >= 0) {
+            combine.add(candidates[idx]);
+            dfs(candidates, target - candidates[idx], ans, combine, idx);
+            combine.remove(combine.size() - 1);
+        }
+    }
+
+}
