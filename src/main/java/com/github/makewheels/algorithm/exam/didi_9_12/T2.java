f78a13851ae7ccb811972cd2b129c8927c32deab
miuser <miuser@email.com>
2021-09-12 19:34:59 +0800

    改包名

--- src/main/java/com/github/makewheels/algorithm/exam/didi_9_12/T2.java
+++
@@ -1,7 +0,0 @@
-package com.github.makewheels.algorithm.exam.didi_9_12;
-
-public class T2 {
-    public static void main(String[] args) {
-        System.out.println(6);
-    }
-}
--- src/main/java/com/github/makewheels/algorithm/exam/tencent_9_5/T5.java
+++
@@ -1,7 +0,0 @@
-package com.github.makewheels.algorithm.exam.tencent_9_5;
-
-public class T5 {
-    public static void main(String[] args) {
-        
-    }
-}
