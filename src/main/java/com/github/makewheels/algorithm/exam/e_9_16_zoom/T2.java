567bfb432d69aa40e1bd128e9fa362dac4bf15d2
miuser <miuser@email.com>
2021-09-16 20:38:17 +0800

    zoom笔试

---
+++ src/main/java/com/github/makewheels/algorithm/exam/e_9_16_zoom/T2.java
@@ -0,0 +1,21 @@
+package com.github.makewheels.algorithm.exam.e_9_16_zoom;
+
+public class T2 {
+    public String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder roman = new StringBuilder();
+        for (int i = 0; i < values.length; i++) {
+            int value = values[i];
+            String symbol = symbols[i];
+            while (num >= value) {
+                num -= value;
+                roman.append(symbol);
+            }
+            if (num == 0) {
+                break;
+            }
+        }
+        return roman.toString();
+    }
+}
