863399286a6bebd15f47005a54a8509049298b33
makewheels <makewheels>
2021-10-13 23:51:42 +0800

    环入口节点

---
+++ src/main/java/com/github/makewheels/algorithm/leetcode/J22.java
@@ -0,0 +1,23 @@
+package com.github.makewheels.algorithm.leetcode;
+
+import com.github.makewheels.algorithm.bean.ListNode;
+
+public class J22 {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                ListNode node = head;
+                while (node != slow) {
+                    node = node.next;
+                    slow = slow.next;
+                }
+                return node;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/github/makewheels/algorithm/nowcoder/NC4.java
@@ -0,0 +1,18 @@
+package com.github.makewheels.algorithm.nowcoder;
+
+import com.github.makewheels.algorithm.bean.ListNode;
+
+public class NC4 {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
