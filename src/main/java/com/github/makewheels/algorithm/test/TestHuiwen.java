017435e17e63603cb4ea5121003421ba5baa464f
makewheels <makewheels>
2021-10-15 22:46:22 +0800

    笔试

---
+++ src/main/java/com/github/makewheels/algorithm/test/TestHuiwen.java
@@ -0,0 +1,8 @@
+package com.github.makewheels.algorithm.test;
+
+public class TestHuiwen {
+    public static void main(String[] args) {
+        int a = 12312;
+
+    }
+}
---
+++ src/main/java/com/github/makewheels/algorithm/test/TestPer.java
@@ -0,0 +1,28 @@
+package com.github.makewheels.algorithm.test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class TestPer {
+
+    private static String[] getPailie(List<Character> characters) {
+        List<List<Character>> list = new ArrayList<>();
+        return null;
+    }
+
+    private static void search(int n, List<Character> in,
+                               List<List<Character>> result, int i) {
+        for (int j = 0; j < n; j++) {
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Character> characters = Arrays.asList('a', 'b', 'c');
+        for (String line : getPailie(characters)) {
+            System.out.println(line);
+        }
+    }
+
+}
