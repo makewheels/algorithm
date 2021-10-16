acc4ca09383ccc93d50963f54ac7127688c604e8
makewheels <makewheels>
2021-10-16 16:59:15 +0800

    笔试

---
+++ src/main/java/com/github/makewheels/algorithm/nowcoder/NC45.java
@@ -0,0 +1,9 @@
+package com.github.makewheels.algorithm.nowcoder;
+
+import com.github.makewheels.algorithm.bean.TreeNode;
+
+public class NC45 {
+    public int[][] threeOrders(TreeNode root) {
+        return null;
+    }
+}
