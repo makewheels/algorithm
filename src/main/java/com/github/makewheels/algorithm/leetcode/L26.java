e98ab4cf7f52c2c5feec1f812bfb674d0b59b8d6
miuser <miuser@email.com>
2021-10-16 21:42:56 +0800

    L26

--- src/main/java/com/github/makewheels/algorithm/leetcode/L26.java
+++ src/main/java/com/github/makewheels/algorithm/leetcode/L26.java
@@ -5,11 +5,13 @@ public class L26 {
         int p1 = 0;
         int p2 = 1;
         while (p2 < nums.length) {
-            if (nums[p1] != nums[p2]) {
+            if (nums[p1] == nums[p2]) {
+                p2++;
+            } else {
                 p1++;
                 nums[p1] = nums[p2];
+                p2++;
             }
-            p2++;
         }
         return p1 + 1;
     }
