c89017096997960a16ca2a527a6b04c96f4badbe
makewheels <makewheels>
2021-10-30 15:41:52 +0800

    来也面试

---
+++ src/main/java/com/github/makewheels/algorithm/interview/laiye_10_30/TestTreeMinDepth.java
@@ -0,0 +1,14 @@
+package com.github.makewheels.algorithm.interview.laiye_10_30;
+
+import com.github.makewheels.algorithm.bean.TreeNode;
+
+public class TestTreeMinDepth {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        int depth = Integer.MAX_VALUE;
+        if (root.left != null) depth = Math.min(minDepth(root.left), depth);
+        if (root.right != null) depth = Math.min(minDepth(root.right), depth);
+        return depth + 1;
+    }
+}
