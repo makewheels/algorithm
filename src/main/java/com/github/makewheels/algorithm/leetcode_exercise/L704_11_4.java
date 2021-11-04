d96692c165742e2139fca3b58e07d63f17be817a
miuser <miuser@email.com>
2021-11-04 11:26:58 +0800

    二分查找

---
+++ src/main/java/com/github/makewheels/algorithm/leetcode_exercise/L704_11_4.java
@@ -0,0 +1,19 @@
+package com.github.makewheels.algorithm.leetcode_exercise;
+
+public class L704_11_4 {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
