560e4f8d4ef192db3844d4485f62ab8a13fd2ee8
makewheels <makewheels>
2021-11-05 23:49:40 +0800

    合并两个有序链表

---
+++ src/main/java/com/github/makewheels/algorithm/leetcode/L19.java
@@ -0,0 +1,9 @@
+package com.github.makewheels.algorithm.leetcode;
+
+import com.github.makewheels.algorithm.basic.bean.ListNode;
+
+public class L19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+    }
+}
--- src/main/java/com/github/makewheels/algorithm/leetcode/L21.java
+++ src/main/java/com/github/makewheels/algorithm/leetcode/L21.java
@@ -4,36 +4,24 @@ import com.github.makewheels.algorithm.basic.bean.ListNode;
 
 public class L21 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode head = null;
-        ListNode each = null;
-        while (l1 != null || l2 != null) {
-            int value;
-            if (l1 == null) {
-                value = l2.val;
-                l2 = l2.next;
-            } else if (l2 == null) {
-                value = l1.val;
+        ListNode dummyHead = new ListNode();
+        ListNode each = dummyHead;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                each.next = l1;
                 l1 = l1.next;
             } else {
-                if (l1.val < l2.val) {
-                    value = l1.val;
-                    l1 = l1.next;
-                } else {
-                    value = l2.val;
-                    l2 = l2.next;
-                }
+                each.next = l2;
+                l2 = l2.next;
             }
-
-            if (head == null) {
-                head = new ListNode(value);
-                each = head;
-            } else {
-                each.next = new ListNode(value);
-                each = each.next;
-            }
-
+            each = each.next;
         }
-        return head;
+        if (l1 != null) {
+            each.next = l1;
+        } else {
+            each.next = l2;
+        }
+        return dummyHead.next;
     }
 
 }
