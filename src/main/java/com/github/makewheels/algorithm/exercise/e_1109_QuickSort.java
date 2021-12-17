c6fa98ea951edd6cf6f90346efa6c4518fa0fcdb
makewheels <makewheels>
2021-12-17 23:07:27 +0800

    commit

--- src/main/java/com/github/makewheels/algorithm/exercise/e_1109_QuickSort.java
+++ src/main/java/com/github/makewheels/algorithm/exercise/e_1109_QuickSort.java
@@ -2,13 +2,7 @@ package com.github.makewheels.algorithm.exercise;
 
 public class e_1109_QuickSort {
     public int[] sortArray(int[] nums) {
-        quickSort(nums, 0, nums.length - 1);
         return nums;
-    }
-
-    private void quickSort(int[] nums, int left, int right) {
-        int p = partiion(nums, left, right);
-
     }
 
     private void partiion(int[] nums, int left, int right) {
--- src/main/java/com/github/makewheels/algorithm/leetcode/L19.java
+++ src/main/java/com/github/makewheels/algorithm/leetcode/L19.java
@@ -4,6 +4,21 @@ import com.github.makewheels.algorithm.basic.bean.ListNode;
 
 public class L19 {
     public ListNode removeNthFromEnd(ListNode head, int n) {
-
+        ListNode fast = new ListNode(0, head);
+        ListNode slow = new ListNode(0, head);
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+        if (fast.next == null) {
+            fast = null;
+            return head;
+        }
+        fast = fast.next;
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        slow.next = slow.next.next;
+        return head;
     }
 }
